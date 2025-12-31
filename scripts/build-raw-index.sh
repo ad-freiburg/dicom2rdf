216666b21a6ac1a4ae3f927ce9d6488244188212
Adem Aydin <mail@adem.dev>
2025-12-31 23:54:39 +0100

    Run `shfmt -w scripts/`

--- scripts/build-raw-index.sh
+++ scripts/build-raw-index.sh
@@ -8,8 +8,8 @@ shopt -u nullglob
 total=${#files[@]}
 
 for i in "${!files[@]}"; do
-  file="${files[$i]}"
-  name=$(basename "$file" .ttl.gz)
-  echo -e "\033[1m[$((i+1))/${total}] Creating index for '$name'\033[0m"
-  QLEVER_NAME="$name" QLEVER_INPUT_FILES="../${file}" qlever index --overwrite-existing
+	file="${files[$i]}"
+	name=$(basename "$file" .ttl.gz)
+	echo -e "\033[1m[$((i + 1))/${total}] Creating index for '$name'\033[0m"
+	QLEVER_NAME="$name" QLEVER_INPUT_FILES="../${file}" qlever index --overwrite-existing
 done
--- scripts/construct.sh
+++ scripts/construct.sh
@@ -8,26 +8,26 @@ shopt -u nullglob
 total=${#files[@]}
 
 for i in "${!files[@]}"; do
-  file="${files[$i]}"
-  name=$(basename "$file" .meta-data.json)
-  suffix="${name##*-}"
-  QLEVER_NAME="$name" qlever start
-  max_depth=$(
-    curl -s -G "http://localhost:7055" \
-      --data-urlencode "query=SELECT ?d WHERE { ?s <meta:maxDepth> ?d }" |
-    jq -r '.results.bindings[0].d.value'
-  )
-  echo -e "\033[1m[$((i+1))/${total}] Constructing semantic triples from '$name' (max depth: $max_depth)\033[0m"
-  /app/construct \
-    --config /app/config.toml \
-    --suffix "$suffix" --output /ttl \
-    --max-depth "$max_depth"
-  QLEVER_NAME="$name" qlever stop
+	file="${files[$i]}"
+	name=$(basename "$file" .meta-data.json)
+	suffix="${name##*-}"
+	QLEVER_NAME="$name" qlever start
+	max_depth=$(
+		curl -s -G "http://localhost:7055" \
+			--data-urlencode "query=SELECT ?d WHERE { ?s <meta:maxDepth> ?d }" |
+			jq -r '.results.bindings[0].d.value'
+	)
+	echo -e "\033[1m[$((i + 1))/${total}] Constructing semantic triples from '$name' (max depth: $max_depth)\033[0m"
+	/app/construct \
+		--config /app/config.toml \
+		--suffix "$suffix" --output /ttl \
+		--max-depth "$max_depth"
+	QLEVER_NAME="$name" qlever stop
 done
 
 (
-  shopt -s nullglob
-  for f in /ttl-static/*.ttl; do
-    gzip -c "$f" >/ttl/$(basename "$f").gz
-  done
+	shopt -s nullglob
+	for f in /ttl-static/*.ttl; do
+		gzip -c "$f" >/ttl/$(basename "$f").gz
+	done
 )
